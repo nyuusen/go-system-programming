76032b81546d6e5cd8854e1b9371dd4411c00c21
nyuusen <pppike.0131@gmail.com>
2026-03-05 11:32:38 +0900

    2-3

--- ch2-3/main.go
+++ ch2-3/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"compress/gzip"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Encoding", "gzip")
+	w.Header().Set("Content-Type", "application/json")
+
+	source := map[string]string{
+		"Hello": "World",
+	}
+
+	// map to json
+	b, err := json.Marshal(source)
+	if err != nil {
+		fmt.Printf("json marshal error: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// multi writer
+	mr := io.MultiWriter(w, os.Stdout)
+
+	// gzip圧縮
+	gw := gzip.NewWriter(mr)
+	defer gw.Flush()
+	if _, err = gw.Write(b); err != nil {
+		fmt.Printf("gzip write error: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
